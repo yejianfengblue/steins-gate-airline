96c8d804b406a8bcae85aa060959af540041e2b4
Jian Feng Ye <yejianfengblue@163.com>
2020-04-07 17:59:34 +0800

    Initial Spring Boot Data Rest + MongoDB project

---
+++ ref-data/src/main/java/com/yejianfengblue/sga/refdata/ReferenceDataApplication.java
@@ -0,0 +1,13 @@
+package com.yejianfengblue.sga.refdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferenceDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReferenceDataApplication.class, args);
+    }
+
+}
---
+++ ref-data/src/test/java/com/yejianfengblue/sga/refdata/ReferenceDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yejianfengblue.sga.refdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferenceDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
