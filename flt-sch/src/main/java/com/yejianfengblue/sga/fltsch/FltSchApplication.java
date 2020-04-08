38b7d8c8a64b14c3ef17046022d3378be745c74a
Jian Feng Ye <yejianfengblue@163.com>
2020-04-08 16:53:42 +0800

    Initial Spring Boot Data Rest + MongoDB project for flt-sch

---
+++ flt-sch/src/main/java/com/yejianfengblue/sga/fltsch/FltSchApplication.java
@@ -0,0 +1,13 @@
+package com.yejianfengblue.sga.fltsch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FltSchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FltSchApplication.class, args);
+    }
+
+}
