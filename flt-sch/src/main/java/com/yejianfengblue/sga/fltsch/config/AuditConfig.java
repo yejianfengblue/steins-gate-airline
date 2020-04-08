afcc28b0c2576d824de692da5fc36db3e7e84c00
Jian Feng Ye <yejianfengblue@163.com>
2020-04-08 17:44:06 +0800

    Enable Mongo auditing

---
+++ flt-sch/src/main/java/com/yejianfengblue/sga/fltsch/config/AuditConfig.java
@@ -0,0 +1,9 @@
+package com.yejianfengblue.sga.fltsch.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@EnableMongoAuditing
+@Configuration
+public class AuditConfig {
+}
