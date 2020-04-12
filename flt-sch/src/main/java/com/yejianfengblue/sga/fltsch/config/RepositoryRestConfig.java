9a0a6a8b122bac1c2bdf3fd5377a2a4671aad9c2
Jian Feng Ye <yejianfengblue@163.com>
2020-04-12 22:24:44 +0800

    Disable PUT for creation and PATCH

--- flt-sch/src/main/java/com/yejianfengblue/sga/fltsch/config/RepositoryRestConfig.java
+++ flt-sch/src/main/java/com/yejianfengblue/sga/fltsch/config/RepositoryRestConfig.java
@@ -1,10 +1,14 @@
 package com.yejianfengblue.sga.fltsch.config;
 
+import com.yejianfengblue.sga.fltsch.flt.Flt;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
+import org.springframework.data.rest.core.mapping.ExposureConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
@@ -20,5 +24,13 @@ public class RepositoryRestConfig implements RepositoryRestConfigurer {
         // enable bean validation before create and save (update)
         validatingListener.addValidator("beforeCreate", beanValidator);
         validatingListener.addValidator("beforeSave", beanValidator);
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration repositoryRestConfiguration) {
+
+        ExposureConfiguration exposureConfiguration = repositoryRestConfiguration.getExposureConfiguration();
+        exposureConfiguration.disablePatchOnItemResources();
+        exposureConfiguration.disablePutForCreation();
     }
 }
