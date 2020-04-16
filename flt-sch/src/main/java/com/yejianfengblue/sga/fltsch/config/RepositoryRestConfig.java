e326b16a8567167881ac4e60686862fff8be6c9c
Jian Feng Ye <yejianfengblue@163.com>
2020-04-16 17:32:58 +0800

    Remove local defined Validator
    Instead use the Spring Boot auto-configured Validator org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration#defaultValidator

--- flt-sch/src/main/java/com/yejianfengblue/sga/fltsch/config/RepositoryRestConfig.java
+++ flt-sch/src/main/java/com/yejianfengblue/sga/fltsch/config/RepositoryRestConfig.java
@@ -7,14 +7,14 @@ import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
 import org.springframework.data.rest.core.mapping.ExposureConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.validation.Validator;
 
 @Configuration
 @RequiredArgsConstructor
 public class RepositoryRestConfig implements RepositoryRestConfigurer {
 
     @NonNull
-    private final LocalValidatorFactoryBean beanValidator;
+    private final Validator beanValidator;
 
     @Override
     public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
