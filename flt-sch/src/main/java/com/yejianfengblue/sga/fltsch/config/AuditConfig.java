54265c5561dda1379b5df2d1c40337165707857a
Jian Feng Ye <yejianfengblue@163.com>
2020-05-17 14:22:37 +0800

    Add an AuditorAware

--- flt-sch/src/main/java/com/yejianfengblue/sga/fltsch/config/AuditConfig.java
+++ flt-sch/src/main/java/com/yejianfengblue/sga/fltsch/config/AuditConfig.java
@@ -1,9 +1,30 @@
 package com.yejianfengblue.sga.fltsch.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @EnableMongoAuditing
 @Configuration
 public class AuditConfig {
+
+    @Component
+    public static class SpringSecurityAuditorAware implements AuditorAware<String>{
+
+        @Override
+        public Optional<String> getCurrentAuditor() {
+
+            return Optional.of(
+                    SecurityContextHolder
+                            .getContext()
+                            .getAuthentication()
+                            .getName());
+        }
+
+    }
+
 }
