891700969faebf817b6b94ab7bcb0b7f36112fc4
Jian Feng Ye <yejianfengblue@163.com>
2020-05-18 17:55:02 +0800

    Customize Spring Data REST config

---
+++ booking/src/main/java/com/yejianfengblue/sga/booking/config/RepositoryRestConfig.java
@@ -0,0 +1,34 @@
+package com.yejianfengblue.sga.booking.config;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
+import org.springframework.data.rest.core.mapping.ExposureConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.validation.Validator;
+
+@Configuration
+@RequiredArgsConstructor
+public class RepositoryRestConfig implements RepositoryRestConfigurer {
+
+    @NonNull
+    private final Validator beanValidator;
+
+    @Override
+    public void configureValidatingRepositoryEventListener(ValidatingRepositoryEventListener validatingListener) {
+
+        // enable bean validation before create and save (update)
+        validatingListener.addValidator("beforeCreate", beanValidator);
+        validatingListener.addValidator("beforeSave", beanValidator);
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration repositoryRestConfiguration) {
+
+        ExposureConfiguration exposureConfiguration = repositoryRestConfiguration.getExposureConfiguration();
+        exposureConfiguration.disablePutForCreation();
+        exposureConfiguration.disablePutOnItemResources();
+    }
+}
