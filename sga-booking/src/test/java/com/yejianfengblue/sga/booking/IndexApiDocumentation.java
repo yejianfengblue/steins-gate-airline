f9053cd2078cfe0908bc96ff9ffa4fe943c70996
Jian Feng Ye <yejianfengblue@163.com>
2020-07-10 15:55:13 +0800

    Revert "Remove unnecessary restdocs about the index response section "_links""
    This reverts commit 3ac1ef1ffd7ac3aa24da66ce4651c531f30f5d4c.

--- sga-booking/src/test/java/com/yejianfengblue/sga/booking/IndexApiDocumentation.java
+++ sga-booking/src/test/java/com/yejianfengblue/sga/booking/IndexApiDocumentation.java
@@ -66,8 +66,10 @@ public class IndexApiDocumentation {
                 .andExpect(status().isOk())
                 .andDo(document("index",
                         links(
-                                linkWithRel("bookings").description("The <<resource_bookings, Bookings resource>>"),
-                                linkWithRel("profile").description("The ALPS profile for this resource"))
+                                linkWithRel("bookings").description("The <<resources_bookings, Bookings resource>>"),
+                                linkWithRel("profile").description("The ALPS profile for this resource")),
+                        responseFields(
+                                subsectionWithPath("_links").description("<<resources_index_access_links, Links>> to other resources"))
                 ));
     }
 
